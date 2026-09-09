471c97f8cc034add545ba82503929c00a5ec5a5d
Reenes <JohnnysDorneys@gmail.com>
2026-09-09 23:44:37 +0300

    feat: not builds, fixed the paths

--- lab3/reporting.sh
+++ lab3/reporting.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$0")"
-LOGGING_FILE="$SCRIPT_DIR/watcher.log"
+LOGGING_FILE="$SCRIPT_DIR/logs/watcher.log"
 DIRECTORY="$SCRIPT_DIR/reports"
 
 mkdir -p "$DIRECTORY"
 
-REPORT_FILE="$DIRECTORY/rep$(date +%Y%m%d:%H%M%S).txt"
+REPORT_FILE="$DIRECTORY/rep$(date +%Y%m%d_%H%M%S).txt"
 
 EVENT_COUNT=$(grep -c "EVENT:" "$LOGGING_FILE")
 
